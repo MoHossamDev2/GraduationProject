3a2f0246a60ac474e667eeccde317ca7f53a2be3
Mohamed Emad Elkhouly <mohamed3mad235@gmail.com>
2025-09-29 10:56:16 +0300

    Initilize Backend Project

---
+++ Backend/GraduationBackend/src/main/java/com/example/GraduationBackend/GraduationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.GraduationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/GraduationBackend/src/test/java/com/example/GraduationBackend/GraduationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GraduationBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
