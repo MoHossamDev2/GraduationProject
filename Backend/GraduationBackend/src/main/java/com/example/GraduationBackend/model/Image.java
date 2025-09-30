cebb256e44a8c2f74ee9902a6c8c95a473654413
Mohamed Emad Elkhouly <mohamed3mad235@gmail.com>
2025-09-30 22:33:30 +0300

    add a model Entity Image whice specifie images of places

--- Backend/GraduationBackend/src/main/java/com/example/GraduationBackend/model/Image.java
+++ Backend/GraduationBackend/src/main/java/com/example/GraduationBackend/model/Image.java
@@ -1,4 +1,41 @@
 package com.example.GraduationBackend.model;
 
-public class image {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+    @Id
+    @Column(name = "image_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id ;
+
+    @Column(name = "file_name")
+    private String fileName ;
+
+    @Column(name = "file_type")
+    private String fileType ;
+
+    @Lob
+    private Blob image ;
+
+    @Column(name = "image_url")
+    private String imageUrl ;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place ;
+
+
+
 }
