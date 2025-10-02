4ab3eb761e7f5d8426a57b804d57c5804b7e26d4
Mohamed Hossam <mhsam2916@gmail.com>
2025-10-02 16:14:44 +0300

    add config

--- Backend/GraduationBackend/src/main/java/com/example/GraduationBackend/configuration/MyDataRestConfig.java
+++ Backend/GraduationBackend/src/main/java/com/example/GraduationBackend/configuration/MyDataRestConfig.java
@@ -1,7 +1,4 @@
 package com.example.GraduationBackend.configuration;
-
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
